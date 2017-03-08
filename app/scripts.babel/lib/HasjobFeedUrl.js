f974543497e4f69ceed5a7a39154587b0eb656a2
Hemant Pawar <hemant@hemantpawar.com>
2017-03-08 21:16:33 +0530

    updated code to functional programming paradigm

--- app/scripts.babel/lib/HasjobFeedUrl.js
+++ app/scripts.babel/lib/HasjobFeedUrl.js
@@ -1,20 +1,8 @@
-function getHostname(url) {
-  const urlObject = new URL(url);
-  const hostname = urlObject.hostname;
+const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x);
 
-  return hostname;
-}
+const createUrlObject = url => new URL(url);
+const getHostname = location => location.hostname;
+const getSubDomain = hostname => (hostname.startsWith('www.') ? hostname.slice(4) : hostname);
+const makeJobFeedUrl = domain => `https://hasjob.co/${domain}/feed`;
 
-function getDomain(url) {
-  const hostname = getHostname(url);
-  const domain = hostname.startsWith('www.') ? hostname.slice(4) : hostname;
-
-  return domain;
-}
-
-export default function getFeedUrl(url) {
-  const domain = getDomain(url);
-
-  return `https://hasjob.co/${domain}/feed`;
-}
-
+export default pipe(createUrlObject, getHostname, getSubDomain, makeJobFeedUrl);
