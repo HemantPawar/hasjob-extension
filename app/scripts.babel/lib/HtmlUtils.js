964ea9826201550a4e01e92d3523a0fab6106592
Hemant Pawar <hemant@hemantpawar.com>
2017-03-17 15:35:15 +0530

    fixed HtmlUtils module to form purity

--- app/scripts.babel/lib/HtmlUtils.js
+++ app/scripts.babel/lib/HtmlUtils.js
@@ -1,19 +1,27 @@
-const jobList = html => `<ul>${html}</ul>`;
-const listItem = job => `<li><a href="${job.link}" target="_blank">${job.title}</a></li>`;
+const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x);
+
+const jobEntries = xml => [...xml.getElementsByTagName('entry')];
+
+const extractJobData = entryList => entryList.map((jobEntry) => {
+  const title = jobEntry.getElementsByTagName('title')[0].childNodes[0].nodeValue;
+  const link = jobEntry.getElementsByTagName('link')[0].getAttribute('href');
+
+  return { link, title };
+});
+
+const createHtml = (jobList) => {
+  if (jobList.length > 0) {
+    const listItem = job => `<li><a href="${job.link}" target="_blank">${job.title}</a></li>`;
+    const htmlJobList = jobList.map(listItem).reduce((a, b) => a + b);
+
+    return `<ul>${htmlJobList}</ul>`;
+  }
+
+  return '<div>Sorry, no jobs listed.</div>';
+};
 
 const textToXml = text => new DOMParser().parseFromString(text, 'text/xml');
-const xmlToHtml = (xml) => {
-  const allJobs = [...xml.getElementsByTagName('entry')];
-
-  const html = allJobs.map((job) => {
-    const title = job.getElementsByTagName('title')[0].childNodes[0].nodeValue;
-    const link = job.getElementsByTagName('link')[0].getAttribute('href');
-
-    return listItem({ link, title });
-  });
-
-  return jobList(html.join(''));
-};
+const xmlToHtml = pipe(jobEntries, extractJobData, createHtml);
 const appendData = (html) => { document.querySelector('.page-content').innerHTML = html; };
 
 export { textToXml, xmlToHtml, appendData };
