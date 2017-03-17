054257b00508e104e7889da1d10fcf2690d5f3b9
Hemant Pawar <hemant@hemantpawar.com>
2017-03-17 14:41:52 +0530

    fixed error message and removed markResolve and markReject to form purity

--- app/scripts.babel/lib/RequestUtils.js
+++ app/scripts.babel/lib/RequestUtils.js
@@ -1,8 +1,9 @@
-const markResolve = response => Promise.resolve(response);
-const markReject = response => Promise.reject(new Error('status not ok', response.statusText));
-
-const checkStatus = response => (response.ok ? markResolve(response) : markReject(response));
+const checkStatus = response => (
+  response.ok ?
+    Promise.resolve(response) :
+    Promise.reject(new Error('status not ok', response.statusText))
+);
 const responseText = response => response.text();
-const handleError = (error) => { console.error('status not ok', error); }; // eslint-disable-line
+const handleError = (error) => { console.error('oops!!!', error); }; // eslint-disable-line
 
 export { checkStatus, responseText, handleError };
