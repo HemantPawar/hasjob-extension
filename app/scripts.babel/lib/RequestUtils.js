e1df5b3ea4f9533a26c1c571554bcd774174cb43
Hemant Pawar <hemant@hemantpawar.com>
2017-03-19 16:53:56 +0530

    added functionality to show error messages in popup

--- app/scripts.babel/lib/RequestUtils.js
+++ app/scripts.babel/lib/RequestUtils.js
@@ -1,9 +1,20 @@
+import pipe from './fp';
+import { appendData } from './HtmlUtils';
+
+const errorMessage = error => (
+  `<div class="page-info">
+    <h3>Something went wrong!!!</h3>
+    <code>${error}</code>
+  </div>`
+);
+
 const checkStatus = response => (
   response.ok ?
     Promise.resolve(response) :
     Promise.reject(new Error('status not ok', response.statusText))
 );
+
 const responseText = response => response.text();
-const handleError = (error) => { console.error('oops!!!', error); }; // eslint-disable-line
+const handleError = pipe(errorMessage, appendData);
 
 export { checkStatus, responseText, handleError };
