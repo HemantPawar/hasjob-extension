3d8738ce3667a8cef55570b0ecdc0620a8fa2789
Hemant Pawar <hemant@hemantpawar.com>
2017-03-25 07:12:15 +0530

    fixed variable name that filters out 'www' from hostname

--- app/scripts.babel/lib/HasjobFeedUrl.js
+++ app/scripts.babel/lib/HasjobFeedUrl.js
@@ -2,7 +2,7 @@ import pipe from './fp';
 
 const createUrlObject = url => new URL(url);
 const getHostname = location => location.hostname;
-const getSubDomain = hostname => (hostname.startsWith('www.') ? hostname.slice(4) : hostname);
+const filterDomain = hostname => (hostname.startsWith('www.') ? hostname.slice(4) : hostname);
 const makeJobFeedUrl = domain => `https://hasjob.co/${domain}/feed`;
 
-export default pipe(createUrlObject, getHostname, getSubDomain, makeJobFeedUrl);
+export default pipe(createUrlObject, getHostname, filterDomain, makeJobFeedUrl);
